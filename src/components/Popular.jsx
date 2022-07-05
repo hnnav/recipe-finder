856bbd8c55e5bbf4b0f469af73b2b6171bc8882e
hnnav <henna.v@icloud.com>
2022-07-05 19:03:05 +0300

    fetch popular recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+
 function Popular() {
 
-    const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+  const [popular, setPopular] = useState([])
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+      const data = await api.json();
+      setPopular(data.recipes)
     }
 
   return (
-    <div>popular</div>
+    <div>
+      {popular.map((recipe) => {
+        return(
+          <div key={recipe.id}>
+            <p> {recipe.title} </p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
